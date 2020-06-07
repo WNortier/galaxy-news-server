02b0259775b083c92a65f21c116c511daccf0ded
warwick <warwick.nortier@gmail.com>
2020-06-07 17:46:23 +0200

    added date function

--- controllers/news-controller.js
+++ controllers/news-controller.js
@@ -1,9 +1,26 @@
 //@ts-nocheck
 const axios = require('axios');
-const apiKey = 'e1e491e99464458eaf0035bca5b520d8';
-const requestUrl = `http://newsapi.org/v2/everything?q=bitcoin&from=2020-05-27&sortBy=publishedAt&apiKey=${apiKey}`;
+
+const buildUrl = () => {
+  const apiKey = 'e1e491e99464458eaf0035bca5b520d8';
+
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  let yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  today = yyyy + '-' + mm + '-' + dd;
+  return `http://newsapi.org/v2/everything?q=bitcoin&from=${today}&sortBy=publishedAt&apiKey=${apiKey}`;
+};
 
 exports.getNews = async (req, res, next) => {
+  let requestUrl = buildUrl();
+
   let results = await axios
     .get(requestUrl)
     .then((response) => {
