c94844c363d5d9471d09c33ef6d7a888c52294c4
wnortier <warwick.nortier@gmail.com>
2021-01-03 01:33:06 +0200

    first commit

--- api/news.js
+++
@@ -1,8 +0,0 @@
-//@ts-nocheck
-const express = require('express');
-const router = express.Router();
-const newsController = require('../controllers/news-controller');
-
-router.get('/get-news', newsController.getNews);
-
-module.exports = router;
